a763456df6da208283b007e0244bb32f06281dce
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-15 10:42:25 +0600

    fix: banner background image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,10 @@ import { Separator } from "@heroui/react";
 
 const Banner = () => {
   return (
-    <div className="bg-[url('/assets/banner.png')] text-white  flex justify-between flex-col items-center  gap-5 h-150">
+     <div
+      style={{ backgroundImage: "url('/assets/banner.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}
+      className="text-white flex justify-between flex-col items-center gap-5 h-150"
+    >
       <div className="p-10 text-center flex justify-center flex-col items-center gap-3.5 flex-1">
         <h1 className="text-7xl">
           Discover Your <br /> Next Adventure

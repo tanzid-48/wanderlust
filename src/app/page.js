ccaba7a5e46cbef4e00055b509bcfeb6e3c6cd0d
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-15 20:19:21 +0600

    chenge layout section

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
  <div>
-<HeroSection></HeroSection>
+{/* <HeroSection></HeroSection> */}
  </div>
   
   );
